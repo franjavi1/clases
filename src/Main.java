9c927d0002a3f70bbb2e32f0334955cad371b23a
franjavi1 <118500511+franjavi1@users.noreply.github.com>
2023-01-29 21:17:17 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        for(int contador =1; contador <= 10; contador=contador+1){
+            System.out.println(contador);}
+    }
+    //Mi código sigue por aquí
+}
